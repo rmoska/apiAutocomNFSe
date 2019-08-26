5716dd76905d1aad9a24abdedf088efb4963a0f3
rmoska <rodrigo@autocominformatica.com.br>
2019-08-26 17:10:45 -0300

    config db flin

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 class Database{
  
  // specify your own database credentials
- private $host = "www.autocominformatica.com.br";
- private $port = "3306";
+ private $host = "localhost";
+ //private $port = "3306";
  private $dbName = "autocom_FC0001";
  private $username = "autocom_FC0001";
  private $password = "atcfc@2608";
@@ -15,7 +15,8 @@ class Database{
      $this->conn = null;
 
      try{
-         $this->conn = new PDO("mysql:host=" . $this->host . ";port=". $this->port . ";dbname=" . $this->dbName, $this->username, $this->password);
+//        $this->conn = new PDO("mysql:host=" . $this->host . ";port=". $this->port . ";dbname=" . $this->dbName, $this->username, $this->password);
+        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->username, $this->password);
          $this->conn->exec("set names utf8");
      }catch(PDOException $exception){
          echo "Connection error: " . $exception->getMessage();
