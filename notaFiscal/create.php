2a03b0477795f934fb61e431122251d22ae3afa8
rmoska <rodrigo@autocominformatica.com.br>
2019-08-28 18:58:52 -0300

    create nf tomador

--- notaFiscal/create.php
+++ notaFiscal/create.php
@@ -10,6 +10,7 @@ include_once '../config/database.php';
 include_once '../config/http_response_code.php';
 include_once '../objects/notaFiscal.php';
 include_once '../objects/notaFiscalItem.php';
+include_once '../objects/tomador.php';
  
 $database = new Database();
 $db = $database->getConnection();
@@ -38,17 +39,83 @@ if(
 ){
     // check / create tomador
     if(
-        !empty($data->idEmitente) &&
-        !empty($data->idVenda) &&
-        !empty($data->valorTotal) &&
-        !empty($data->documento) &&
-        !empty($data->codigoMunicipio) &&
-        !empty($data->email) 
+        !empty($data->tomador->documento) &&
+        !empty($data->tomador->nome) &&
+        !empty($data->tomador->logradouro) &&
+        !empty($data->tomador->numero) &&
+        !empty($data->tomador->complemento) &&
+        !empty($data->tomador->bairro) &&
+        !empty($data->tomador->cep) &&
+        !empty($data->tomador->codigoMunicipio) &&
+        !empty($data->tomador->uf) &&
+        !empty($data->tomador->email) 
     ){
+
+        $tomador = new Tomador($db);
+
+        // set tomador property values
+        $tomador->documento = $data->tomador->documento;
+        $tomador->nome = $data->tomador->nome;
+        $tomador->logradouro = $data->tomador->logradouro;
+        $tomador->numero = $data->tomador->numero;
+        $tomador->complemento = $data->tomador->complemento;
+        $tomador->bairro = $data->tomador->bairro;
+        $tomador->cep = $data->tomador->cep;
+        $tomador->codigoMunicipio = $data->tomador->codigoMunicipio;
+        $tomador->uf = $data->tomador->uf;
+        $tomador->email = $data->tomador->email;
+    
+    if ($tomador->check() > 0) {
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $tomador->idTomador = $row['idEmitente'];
+
+    }
+    // create emitente
+    else if($emitente->create()){
+ 
+        // set response code - 201 created
+        http_response_code(201);
+ 
+        // tell the user
+        echo json_encode(array("http_code" => "201", "message" => "Emitente incluído", "idEmitente" => $emitente->idEmitente));
+    }
+ 
+    // if unable to create emitente, tell the user
+    else{
+ 
+        // set response code - 503 service unavailable
+        http_response_code(503);
+ 
+        // tell the user
+        echo json_encode(array("http_code" => "503", "message" => "Não foi possível incluir Emitente. Serviço indisponível."));
+    }
+}
+
+
+
+
+
+    }
     
 
-
     //check / create itemVenda
+    foreach ( $data->itemVenda as $item )
+    {
+
+        if(
+            !empty($item->codigo) &&
+            !empty($item->descricao) &&
+            !empty($item->cnae) &&
+            !empty($item->nbs) &&
+            !empty($item->quantidade) &&
+            !empty($item->valor) &&
+            !empty($item->txIss) 
+        ){
+    
+        }
+    }
+
 
 
 
