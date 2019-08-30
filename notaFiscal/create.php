e68ffcf0e7672e06c6b5188d6958c7f2cade984d
rmoska <rodrigo@autocominformatica.com.br>
2019-08-30 17:57:21 -0300

    cstiss

--- notaFiscal/create.php
+++ notaFiscal/create.php
@@ -244,7 +244,7 @@ if(
 
 			$xml->startElement("itemServico");
 			$xml->writeElement("aliquota", number_format(($notaFiscalItem->taxaIss/100),4,'.',''));
-			$xml->writeElement("cst", $notaFiscalItem->cst);
+			$xml->writeElement("cst", $notaFiscalItem->cstIss);
             //
             
 //            $nmProd = trim(limpaCaractNFe(retiraAcentos($notaFiscalItem->descricao)));
