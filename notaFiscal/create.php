6fb06ea9202341e08f7ae0992217e1b8188a6d91
rmoska <rodrigo@autocominformatica.com.br>
2019-09-02 14:07:24 -0300

    shared

--- notaFiscal/create.php
+++ notaFiscal/create.php
@@ -339,7 +339,7 @@ if(
             $notaFiscal->chaveNF = $cdVerif;
             $notaFiscal->situacao = "F";
             $notaFiscal->dataProcessamento = $dtProc;
-            include_once '../notaFiscal/printNFSe.php';
+            include_once '../shared/printNFSe.php';
             $dNFSe = danfeNFe($notaFiscal->idNotaFiscal, $db);
             //            $notaFiscal->gerarPDF();
 
