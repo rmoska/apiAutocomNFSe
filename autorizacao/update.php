2b631b7b2a6a6aee20467bdea4b819cb61eac0be
rmoska <rodrigo@autocominformatica.com.br>
2019-09-05 14:02:37 -0300

    upd auto

--- autorizacao/update.php
+++ autorizacao/update.php
@@ -68,9 +68,15 @@ if(
             exit;
         }
         else {
+
             include_once '../comunicacao/signNFSe.php';
             $arraySign = array("cnpj" => $emitente->documento, "keyPass" => $autorizacao->senha);
             $certificado = new SignNFSe($arraySign);
+            if (!is_object($certificado)){
+                http_response_code(503);
+                echo json_encode(array("http_code" => "503", "message" => "Não foi possível incluir Certificado.", "erro" => $certificado));
+                exit;
+            }
             $validade = $certificado->certDaysToExpire;
         }
 
