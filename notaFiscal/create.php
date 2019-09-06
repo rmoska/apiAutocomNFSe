5b527e55c706d76f6b211535701348372bc30a29
rmoska <rodrigo@autocominformatica.com.br>
2019-09-06 10:38:24 -0300

    msg valor

--- notaFiscal/create.php
+++ notaFiscal/create.php
@@ -240,7 +240,7 @@ foreach ( $data->itemServico as $item )
     if (number_format($totalItens,2,'.','') != number_format($notaFiscal->valorTotal,2,'.','')){
         http_response_code(503);
         echo json_encode(array("http_code" => "503", "message" => "Não foi possível incluir Nota Fiscal.(NFi02)", 
-                               "erro" => "Valor dos itens não fecha com Valor Total da Nota. (".number_format($totalItens,2,'.','') != number_format($notaFiscal->valorTotal,2,'.','')." )"));
+                               "erro" => "Valor dos itens não fecha com Valor Total da Nota. (".number_format($totalItens,2,'.','')." <> ".number_format($notaFiscal->valorTotal,2,'.','')." )"));
         $db->rollBack();
         exit;
     }
