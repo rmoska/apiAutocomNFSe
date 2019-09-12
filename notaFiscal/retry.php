dca9b486d255ab26853aa4e49c52fe198ef275b5
rmoska <rodrigo@autocominformatica.com.br>
2019-09-12 14:09:14 -0300

    lc utf

--- notaFiscal/retry.php
+++ notaFiscal/retry.php
@@ -1,6 +1,7 @@
 <?php
 
-setlocale(LC_ALL, "pt_BR", "pt_BR.iso-8859-1", "pt_BR.utf-8", "portuguese");
+setlocale(LC_ALL, "pt_BR.utf-8");
+//setlocale(LC_ALL, "pt_BR.iso-8859-1", "pt_BR.utf-8");
 
 // Classe para repetir tentativa de emissão de NFSe PMF pendentes por Servidor Indisponível / Timeout
 
