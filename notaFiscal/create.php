d6ccc9a24ba576745bac9967572b3364e2c68798
rmoska <rodrigo@autocominformatica.com.br>
2019-09-20 11:01:57 -0300

    upd nf

--- notaFiscal/create.php
+++ notaFiscal/create.php
@@ -9,21 +9,61 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
+include_once '../config/database.php';
+include_once '../shared/http_response_code.php';
+include_once '../objects/emitente.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
 //
-$codigoMunicipio = $_GET['idMunicipio'];
+if(empty($data->documento)) {
 
-if (!isset($codigoMunicipio)) {
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Não foi possível emitir Nota Fiscal. Emitente não identificado."));
+    $strData = json_encode($data);
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Não foi possível emitir Nota Fiscal. Emitente não identificado. ".$strData."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
+}
 
-    echo json_encode(array("http_code" => "400", "message" => "Parâmetro idMunicipio não informado"));
+//
+$emitente = new Emitente($db);
+$emitente->documento = $data->documento;
+if (($idEmitente = $emitente->check()) > 0) {
+    $notaFiscal->idEmitente = $idEmitente;
+}
+else{
+
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Emitente não cadastrado. Nota Fiscal não pode ser emitida."));
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Emitente não cadastrado. Nota Fiscal não pode ser emitida. Emitente=".$data->documento."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
+}
+$emitente->idEmitente = $idEmitente;
+$emitente->readOne();
+
+if (!isset($emitente->codigoMunicipio)) {
+
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Emitente sem Município definido no cadastro."));
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Emitente sem Município definido no cadastro. Emitente=".$data->idEmitente."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
+}
+
+$fileClass = './'.$emitente->codigoMunicipio.'/create.php';
+if (file_exists($fileClass)) {
+
+    include $fileClass;
 }
 else {
 
-    $fileClass = './'.$codigoMunicipio.'/create.php';
-    if (file_exists($fileClass))
-        include $fileClass;
-    else 
-        echo json_encode(array("http_code" => "400", "message" => "Município não disponível para emissão da NFSe"));
-
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Município não disponível para emissão da NFSe."));
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Município não disponível para emissão da NFSe. Município=".$emitente->codigoMunicipio."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
 }
 
 ?>
