3173865393250684ad99acbb95690fe9e8ce5e1d
rmoska <rodrigo@autocominformatica.com.br>
2019-09-20 11:24:17 -0300

    pdf

--- notaFiscal/gerarPdf.php
+++ notaFiscal/gerarPdf.php
@@ -9,61 +9,83 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
 include_once '../shared/http_response_code.php';
 include_once '../objects/notaFiscal.php';
+include_once '../objects/emitente.php';
 //
-$codigoMunicipio = $_GET['idMunicipio'];
 
-if (!isset($codigoMunicipio)) {
+$idNotaFiscal = $_GET['idNotaFiscal'];
 
-    echo json_encode(array("http_code" => "400", "message" => "Parâmetro idMunicipio não informado"));
+if (!isset($idNotaFiscal)) {
+
+    echo json_encode(array("http_code" => "400", "message" => "Parâmetro idNotaFiscal não informado"));
+    exit;
+}
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+$notaFiscal = new NotaFiscal($db);
+$notaFiscal->idNotaFiscal = $_GET['idNotaFiscal'];
+
+$checkNF = $notaFiscal->check();
+if ($checkNF["existe"] == 0) {
+
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", 
+                            "message" => "Nota Fiscal não encontrada. idNotaFiscal=".$notaFiscal->idNotaFiscal));
+    exit;
+}
+
+$notaFiscal->readOne();
+
+$emitente = new Emitente($db);
+$emitente->idEmitente = $notaFiscal->idEmitente;
+$emitente->readOne();
+
+if (is_null($emitente->documento)) {
+
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Emitente não cadastrado para esta Autorização."));
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Emitente não cadastrado para esta Autorização. Emitente=".$data->idEmitente."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
+}
+
+if (!isset($emitente->codigoMunicipio)) {
+
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Emitente sem Município definido no cadastro."));
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Emitente sem Município definido no cadastro. Emitente=".$data->idEmitente."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
+}
+
+
+$fileClass = './'.$emitente->codigoMunicipio.'/gerarPdf.php';
+if (file_exists($fileClass)) {
+
+    include $fileClass;
+
+    $gerarPdf = new gerarPdf();
+
+    if($arqPDF = $gerarPdf->printDanfpse($notaFiscal->idNotaFiscal, $db)){
+
+        http_response_code(200);
+        echo json_encode(array("http_code" => "200", 
+                                "message" => "Arquivo PDF criado", "linkPDF" => "http://www.autocominformatica.com.br/apiAutocomNFSe/".$arqPDF));
+    }
+    else{
+    
+        http_response_code(500);
+        echo json_encode(array("http_code" => "500", 
+                                "message" => "Não foi possível gerar arquivo PDF. idNotaFiscal=".$notaFiscal->idNotaFiscal));
+    }
+    
 }
 else {
 
-    $fileClass = './'.$codigoMunicipio.'/gerarPdf.php';
-    if (file_exists($fileClass)) {
-        include $fileClass;
-
-        // get database connection
-        $database = new Database();
-        $db = $database->getConnection();
-        
-        $notaFiscal = new NotaFiscal($db);
-        
-        $notaFiscal->idNotaFiscal = $_GET['idNotaFiscal'];
-
-        $checkNF = $notaFiscal->check();
-        if ($checkNF["existe"] == 0) {
-
-            http_response_code(400);
-            echo json_encode(array("http_code" => "400", 
-                                    "message" => "Nota Fiscal não encontrada. idNotaFiscal=".$notaFiscal->idNotaFiscal));
-        }
-        else{
-
-            $gerarPdf = new gerarPdf();
-
-            if($arqPDF = $gerarPdf->printDanfpse($notaFiscal->idNotaFiscal, $db)){
-    
-                http_response_code(200);
-                echo json_encode(array("http_code" => "200", 
-                                        "message" => "Arquivo PDF criado", "linkPDF" => "http://www.autocominformatica.com.br/apiAutocomNFSe/".$arqPDF));
-            }
-            
-            // if unable to delete emitente
-            else{
-            
-                http_response_code(500);
-                echo json_encode(array("http_code" => "500", 
-                                        "message" => "Não foi possível gerar arquivo PDF. idNotaFiscal=".$notaFiscal->idNotaFiscal));
-            }
-        
-
-        }
-
-
-    }
-    else 
-        echo json_encode(array("http_code" => "400", "message" => "Município não disponível para emissão da NFSe"));
-
+    http_response_code(400);
+    echo json_encode(array("http_code" => "400", "message" => "Município não disponível para emissão da NFSe."));
+    error_log(utf8_decode("[".date("Y-m-d H:i:s")."] Município não disponível para emissão da NFSe. Município=".$emitente->codigoMunicipio."\n"), 3, "../arquivosNFSe/apiErrors.log");
+    exit;
 }
 
 ?>
