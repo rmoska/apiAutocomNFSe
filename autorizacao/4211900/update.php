0d708efd8cfd7a4eed4d9c233b52ef4fad7221fc
rmoska <rodrigo@autocominformatica.com.br>
2019-09-27 14:44:20 -0300

    arq

--- autorizacao/4211900/update.php
+++ autorizacao/4211900/update.php
@@ -113,6 +113,8 @@ if(
         // transmite NFSe	
         $headers = array( "Content-type: application/xml" ); 
 
+        echo $arqNFSe;
+
         $params = "login=".$emitente->documento."&senha=".$autorizacao->senhaWeb."&cidade=8233&fl=".$arqNFSe;
 
 echo $params;
