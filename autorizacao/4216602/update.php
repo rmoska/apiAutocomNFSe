f8aed6287294fa1e56cbf1f8b98fc40fb3e0e38b
rmoska <rodrigo@autocominformatica.com.br>
2019-10-02 16:11:42 -0300

    aliq

--- autorizacao/4216602/update.php
+++ autorizacao/4216602/update.php
@@ -87,6 +87,7 @@ if(
                         $xml->endElement(); // Valores
                         $xml->writeElement("IssRetido", 2);
                         $xml->writeElement("ItemListaServico", $aAutoChave["codigoServico"]); //"0402");
+                        $xml->writeElement("Aliquota", 0.00); // 4216602
                         $xml->writeElement("Discriminacao", "Consulta clinica");
                         $xml->writeElement("CodigoMunicipio", 0); // 4216602
                         $xml->writeElement("ExigibilidadeISS", 1);
