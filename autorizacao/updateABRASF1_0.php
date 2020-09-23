e57c63f3cc46c8490b92426ff24196e30353732b
rmoska <rodrigo.moskorz@gmail.com>
2020-09-23 11:13:30 -0300

    cdv4

--- autorizacao/updateABRASF1_0.php
+++ autorizacao/updateABRASF1_0.php
@@ -256,10 +256,15 @@ if($retorno[0]){
     }
 
     http_response_code(201);
-    echo json_encode(array("http_code" => 201, "message" => "Autorização atualizada OK", 
+
+echo $cdVerif;
+
+$cdVerif = 'OK';
+
+    echo json_encode(array("http_code" => 201, "message" => "Autorização atualizada", 
                            "validade" => $validade." dias",
                            "nf-homolog" => $nuNF,
-                           "verificacao-homolog" => $cdVerif,
+                           "verificacao-homolog" => utf8_decode($cdVerif),
                            "linkNF" => $linkNF));
     exit;
 }
