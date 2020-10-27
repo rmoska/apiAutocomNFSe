265881868a24328fed9f226d5f7b7c2aacb4ee28
rmoska <rodrigo.moskorz@gmail.com>
2020-10-27 16:36:05 -0300

    tag ass

--- autorizacao/updateSINTESE.php
+++ autorizacao/updateSINTESE.php
@@ -137,7 +137,7 @@ if($retorno[0]){
 
     $xmlRps = $xml->outputMemory(true);
 
-    $xmlAss = $objNFSe->signXML($xmlRps, 'Rps', '');
+    $xmlAss = $objNFSe->signXML($xmlRps, 'InfDeclaracaoPrestacaoServico', 'Rps');
     if ($objNFSe->errStatus) {
 
         http_response_code(401);
