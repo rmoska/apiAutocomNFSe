3637f0300414030fea37cfc5d635f9028d4c5223
rmoska <rodrigo.moskorz@gmail.com>
2020-11-16 16:34:51 -0300

    dtval

--- autorizacao/updateFLN.php
+++ autorizacao/updateFLN.php
@@ -79,7 +79,7 @@ if($retorno[0]){
         $dataValidade = new DateTime(date('Y-m-d'));
         $dataValidade->add(new DateInterval('P'.$validade.'D'));
 
-        error_log(("[".date("Y-m-d H:i:s")."]." ".$dataValidade."\n"), 3, "../arquivosNFSe/apiErrors.log");
+        error_log(("[".date("Y-m-d H:i:s")."] ".$dataValidade."\n"), 3, "../arquivosNFSe/apiErrors.log");
 
         $autorizacao->dataValidade = $dataValidade;
     }
