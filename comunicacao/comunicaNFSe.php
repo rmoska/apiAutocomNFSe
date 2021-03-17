ed63e3b2cc70dab1bac73ae24f1c3e65731ae9ce
rmoska <rodrigo.moskorz@gmail.com>
2021-03-17 16:36:25 -0300

    cab

--- comunicacao/comunicaNFSe.php
+++ comunicacao/comunicaNFSe.php
@@ -767,7 +767,7 @@ error_log(utf8_decode("[".date("Y-m-d H:i:s")."] RETORNO=".implode($retorno)."\n
 
             <e:RecepcionarLoteRPS.Execute>
                 <e:Nfsecabecmsg>
-                    <cabecalho xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" versao="201001">
+                    <cabecalho versao=201001>
                         <versaoDados>V2010</versaoDados>
                     </cabecalho>
                 </e:Nfsecabecmsg>
