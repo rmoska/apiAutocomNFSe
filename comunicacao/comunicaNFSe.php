661e6758a96e7f4492989db77dffd70492c9c78c
rmoska <rodrigo.moskorz@gmail.com>
2021-03-25 10:22:28 -0300

    cdata

--- comunicacao/comunicaNFSe.php
+++ comunicacao/comunicaNFSe.php
@@ -779,8 +779,8 @@ error_log(utf8_decode("[".date("Y-m-d H:i:s")."] RETORNO=".implode($retorno)."\n
                      <soapenv:Header/>
                      <soapenv:Body>
                      <e:RecepcionarLoteRPS.Execute>';
-            $data .= '<e:Nfsecabecmsg><cabecalho versao="201001"><versaoDados>V2010</versaoDados></cabecalho></e:Nfsecabecmsg>';
-            $data .= '<e:Nfsedadosmsg>'.$sNFSe.'</e:Nfsedadosmsg>';
+            $data .= '<e:Nfsecabecmsg><![CDATA[<cabecalho versao="201001"><versaoDados>V2010</versaoDados></cabecalho>]]</e:Nfsecabecmsg>';
+            $data .= '<e:Nfsedadosmsg><![CDATA['.$sNFSe.']]</e:Nfsedadosmsg>';
             $data .= '</e:RecepcionarLoteRPS.Execute>
                      </soapenv:Body>
                      </soapenv:Envelope>';
